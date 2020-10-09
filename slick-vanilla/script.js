344f4d3cc93f2cf892e322c56f8f522f809bf833
Chinh Phan <chinh.phan@dfo.global>
2020-10-09 11:02:37 +0700

    d

--- slick-vanilla/script.js
+++ slick-vanilla/script.js
@@ -1,1 +1,21 @@
+new Blazy({
+    selector: 'img:not(.no-lazy)', // all images
+    loadInvisible: true,
+    offset: 1000,
+    breakpoints: [{
+        width: 767, // max-width
+        src: 'data-src-small'
+    },
+    {
+        width: 991, // max-width
+        src: 'data-src-medium'
+    }
+    ],
+    error: function (ele, msg) {
+        console.log('lazy load image error: ', ele.src, ': ', msg)
+    },
+    success: function (ele) {
+        console.log('success: ', ele);
+    }
+});
 // new Slick(document.querySelector('.slider .w_inner'), {infinite: false});
