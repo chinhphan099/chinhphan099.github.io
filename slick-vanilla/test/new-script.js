30253113e9b562c2289fee3bb0027ca5b7338d16
Chinh Phan <chinh.phan@dfo.global>
2020-11-24 17:41:33 +0700

    slick 1.8.0

--- slick-vanilla/test/new-script.js
+++ slick-vanilla/test/new-script.js
@@ -1,7 +1,7 @@
-// var commentSlick = new Slick(document.querySelector('.main-slider'), {infinite: true});
+// var commentSlick = new Slick(document.querySelector('.main-slider'), {infinite: true, dots: true});
 
 // commentSlick.unslick();
 $('.main-slider').slick({
-    "slidesToShow": 4,
-    "slidesToScroll": 4
+    "slidesToShow": 2,
+    "slidesToScroll": 2
 });
