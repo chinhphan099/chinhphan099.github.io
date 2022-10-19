aefabfef323b9b003ae1a7c69841e8d0f1b4aa3a
Chinh Phan <chinhphan099@gmail.com>
2022-10-18 20:38:55 -0400

    publish queenbee project to github.io

---
+++ queenbee/js/jsguide.js
@@ -0,0 +1,2 @@
+"use strict";var Guide=function(n,e,i){return{win:n(e),doc:n(document),html:n("html"),body:n("body"),resize:"onorientationchange"in e?"orientationchange.resizeWindow":"resize.resizeWindow",globalFct:function(){}}}(jQuery,window);jQuery(function(){Guide.globalFct()});
+//# sourceMappingURL=jsguide.js.map
---
+++ queenbee/js/l10n.js
@@ -0,0 +1,6 @@
+var L10n = {
+  validateMess: {
+    cemail: 'Invalid Email',
+    minlengthcheckbox: 'Please chose at least {0} items.'
+  }
+};
