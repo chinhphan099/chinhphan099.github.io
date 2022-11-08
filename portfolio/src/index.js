fd6819d227130bd17f7b21af6817a7ea02818c80
Chinh Phan <chinhphan099@gmail.com>
2022-11-07 21:37:50 -0500

    Convert to function component

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,35 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import myData from './data.json'
 
-class Portfolio extends React.Component {
-  constructor(props) {
-    super(props);
-    this.history = {}
-  }
-  shouldComponentUpdate() {
-    console.log('3. shouldComponentUpdate - Before change Props/State')
-    //! Must return true or false
-    return true
-    //* true: will re-render component
-    //? return false
-    //* false: not re-render component
-  }
-  componentDidUpdate() {
-    console.log('4. componentDidUpdate - After Re-Render Done')
-  }
-  componentDidMount() {
-    console.log('2. componentDidMount - Render Done')
-  }
-  render() {
-    return (
-      <div className="wrapper">
-        <header>
-          <h1>Hi, My name is {myData.name}</h1>
-          <p>{myData.position}</p>
-        </header>
-      </div>
-    );
-  }
+export default function Portfolio() {
+  return (
+    <div className="wrapper">
+      <header>
+        <h1>Hi, My name is {myData.name}</h1>
+        <p>{myData.position}</p>
+        <img src={myData.avt} />
+      </header>
+    </div>
+  );
 }
 
 // ========================================
